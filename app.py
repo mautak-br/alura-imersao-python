1b8080fffe84715d782b07e1da6567ac16a7e1dd
mautak-br <mautak@gmail.com>
2025-08-08 18:43:28 -0300

    Corrigindo comentários

--- app.py
+++ app.py
@@ -11,9 +11,8 @@ st.set_page_config(
 )
 
 # --- Carregamento dos dados ---
-'''
-df = pd.read_csv("https://raw.githubusercontent.com/vqrca/dashboard_salarios_dados/refs/heads/main/dados-imersao-final.csv")
-'''
+
+#df = pd.read_csv("https://raw.githubusercontent.com/vqrca/dashboard_salarios_dados/refs/heads/main/dados-imersao-final.csv")
 df = pd.read_csv("dados-imersao-final.csv")
 
 # --- Barra Lateral (Filtros) ---
